344ce62067225f35ad6068241e4d8404145561f0
RichDi <drdr_2@hotmail.com>
2017-10-12 16:15:07 -0500

    Modified Login and Added Styles

--- register/urls.py
+++ register/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'register'
 
 urlpatterns = [    
-    url(r'^login/$', views.login_view, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^login/$', views.logout_view, name='logout'),
     url(r'^register/$', views.register, name='register'),    
 ]
