f907fe700049daf697a122ced72891f7c620cbbe
Jonathan Basilio <softwaremanager@digimundo.com.mx>
2018-04-04 14:48:14 -0500

    add url

--- tv_mundo/static/javascript/main.js
+++ tv_mundo/static/javascript/main.js
@@ -2,7 +2,7 @@
     function envia_ajax(cardIds) {
       console.log(cardIds);
       var myRequest = new Request.HTML({
-        url: urltarot,
+        url: /start/,
         method: "post",
         data: {
             'cartas': cardIds
