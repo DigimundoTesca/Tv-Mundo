ca616d39a3cdff924c91323f859d9fc6c117ed81
RichDi <drdr_2@hotmail.com>
2018-06-06 03:10:26 -0500

    Added Paypal Screen and functions

--- tv_mundo/static/javascript/effects.js
+++ tv_mundo/static/javascript/effects.js
@@ -1,18 +1,15 @@
 /*background aleatorio*/
-do {
-  $(document).ready(function() {
-
-  var contenedor = $('#inicio');
-  var imagenes = ['01.JPG', '02.JPG', '03.jpg', '04.JPG', '05.JPG', '06.JPG'];
-  var tiempo = 3500;
-  contenedor.css({'background-image':'url(../static/images/metamundo/03.jpg)'});
-
-  function image(){
-  setTimeout(function() {
+var contenedor = $('#inicio');
+var imagenes = ['01.JPG', '02.JPG', '03.jpg', '04.JPG', '05.JPG', '06.JPG'];
+var tiempo = 3500;
+contenedor.css({'background-image':'url(../static/images/metamundo/03.jpg)'});
+image();
+function image(){
   contenedor.fadeIn('slow', 0.5, function() {
-  $(this).css({'background-image': 'url(../static/images/metamundo/' + imagenes[Math.floor(Math.random() * imagenes.length)] + ')'});
-  image();}).fadeIn('slow', 0.5); },tiempo); }
-
-  image();
-  });
-} while (false);
+    var img = new Image();
+    var random = imagenes[Math.floor(Math.random() * imagenes.length)]
+    img.src = '/static/images/metamundo/' + random ;        
+    $(this).css({'background-image': 'url(../static/images/metamundo/' + random + ')'});        
+  });         
+  setTimeout(image,tiempo);
+};
